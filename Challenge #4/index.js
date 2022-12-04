f01ae901aae7b4ee582adda3864e897b48301e18
charlsdev <youquince@gmail.com>
2022-12-04 18:11:14 -0500

    Add solution of the challenge four

---
+++ Challenge #4/index.js
@@ -0,0 +1,32 @@
+const boxes = [
+   { l: 1, w: 1, h: 1 },
+   { l: 2, w: 2, h: 2 }
+]
+
+function fitsInOneBox(boxes) {
+   var resp
+   
+   for (let a = 1; a < boxes.length; a++) {
+      if(
+         (boxes[a - 1].l > boxes[a].l) && 
+         (boxes[a - 1].w > boxes[a].w) && 
+         (boxes[a - 1].h > boxes[a].h)
+      ) {
+         resp = true
+      } else {
+         if(
+         (boxes[a - 1].l >= boxes[a].l) || 
+         (boxes[a - 1].w >= boxes[a].w) || 
+         (boxes[a - 1].h >= boxes[a].h)
+         ) {
+         resp = false
+         } else {
+         resp = true
+         }
+      }
+   }
+   
+   return resp
+}
+
+fitsInOneBox(boxes) // false
