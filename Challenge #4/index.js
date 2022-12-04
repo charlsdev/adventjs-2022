4ee3c50e70bcfd6f226509992c375f9c50c96551
charlsdev <youquince@gmail.com>
2022-12-04 18:16:20 -0500

    Add solution of the challenge four

--- Challenge #4/index.js
+++ Challenge #4/index.js
@@ -1,32 +1,33 @@
 const boxes = [
-   { l: 1, w: 1, h: 1 },
-   { l: 2, w: 2, h: 2 }
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 }
 ]
 
 function fitsInOneBox(boxes) {
-   var resp
-   
-   for (let a = 1; a < boxes.length; a++) {
+  var resp
+  
+  for (let a = 1; a < boxes.length; a++) {
+    if(
+      (boxes[a - 1].l > boxes[a].l) && 
+      (boxes[a - 1].w > boxes[a].w) && 
+      (boxes[a - 1].h > boxes[a].h)
+    ) {
+      resp = true
+    } else {
       if(
-         (boxes[a - 1].l > boxes[a].l) && 
-         (boxes[a - 1].w > boxes[a].w) && 
-         (boxes[a - 1].h > boxes[a].h)
+        (boxes[a - 1].l >= boxes[a].l) || 
+        (boxes[a - 1].w >= boxes[a].w) || 
+        (boxes[a - 1].h >= boxes[a].h)
       ) {
-         resp = true
+        resp = false
       } else {
-         if(
-         (boxes[a - 1].l >= boxes[a].l) || 
-         (boxes[a - 1].w >= boxes[a].w) || 
-         (boxes[a - 1].h >= boxes[a].h)
-         ) {
-         resp = false
-         } else {
-         resp = true
-         }
+        resp = true
       }
-   }
-   
-   return resp
+    }
+  }
+  
+  return resp
 }
 
 fitsInOneBox(boxes) // false
