9e91aa55484542f4937483f61713999ddddce57f
charlsdev <youquince@gmail.com>
2023-01-14 18:40:52 -0500

    Fix spacing

--- Challenge #4/index.js
+++ Challenge #4/index.js
@@ -1,33 +1,33 @@
 const boxes = [
-  { l: 1, w: 1, h: 1 },
-  { l: 2, w: 2, h: 2 },
-  { l: 3, w: 1, h: 3 }
+   { l: 1, w: 1, h: 1 },
+   { l: 2, w: 2, h: 2 },
+   { l: 3, w: 1, h: 3 }
 ]
 
 function fitsInOneBox(boxes) {
-  var resp
-  
-  for (let a = 1; a < boxes.length; a++) {
-    if(
-      (boxes[a - 1].l > boxes[a].l) && 
-      (boxes[a - 1].w > boxes[a].w) && 
-      (boxes[a - 1].h > boxes[a].h)
-    ) {
-      resp = true
-    } else {
+   var resp
+   
+   for (let a = 1; a < boxes.length; a++) {
       if(
-        (boxes[a - 1].l >= boxes[a].l) || 
-        (boxes[a - 1].w >= boxes[a].w) || 
-        (boxes[a - 1].h >= boxes[a].h)
+         (boxes[a - 1].l > boxes[a].l) && 
+         (boxes[a - 1].w > boxes[a].w) && 
+         (boxes[a - 1].h > boxes[a].h)
       ) {
-        resp = false
+         resp = true
       } else {
-        resp = true
+         if(
+         (boxes[a - 1].l >= boxes[a].l) || 
+         (boxes[a - 1].w >= boxes[a].w) || 
+         (boxes[a - 1].h >= boxes[a].h)
+         ) {
+         resp = false
+         } else {
+         resp = true
+         }
       }
-    }
-  }
-  
-  return resp
+   }
+   
+   return resp
 }
 
 fitsInOneBox(boxes) // false
